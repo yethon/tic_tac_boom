84bbb35b562f32fb22b4afd08255bbdb39ac7817
yethon <ursula.yethon@mheducation.com>
2015-11-23 16:18:50 -0500

    More brute force logic... baby steps...

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var TicTacBoom = (function() {
   var app = {};
   var player = 'X';
+  var board = document.getElementById("board");
   
   function _updateBoard ( event ) {
     event.srcElement.innerHTML = player;
@@ -11,36 +12,55 @@ var TicTacBoom = (function() {
     return a0 === a1 && a1 === a2;
   }
   
-  function _winningRow(/**/) {
-    // return _isWinner (a0, a1, a2);
+  // dry this out
+  function _winningRow(row) {
+    var first = $('tr').eq(row).children()[0].innerHTML;
+    var second = $('tr').eq(row).children()[1].innerHTML;
+    var third = $('tr').eq(row).children()[2].innerHTML;
+    
+    return _isWinner(first, second, third);
   }
   
-  function _winningColumn(/**/) {
-    // return _isWinner (a0, a1, a2);
+  // dry this out
+  function _winningColumn(col) {
+    var first = $('tr').eq(0).children()[col].innerHTML;
+    var second = $('tr').eq(1).children()[col].innerHTML;
+    var third = $('tr').eq(2).children()[col].innerHTML;
+    
+    return _isWinner(first, second, third);
   }
   
-  function _winningDiagonal(/**/) {
-    // return _isWinner (a0, a1, a2);
+  function _winningDiagonal(row, col) {
+    col = parseInt(col);
+    row = parseInt(row);
+    
+    if((col === 0 && row === 1) ||
+       (col === 2 && row === 1) ||
+       (col === 1 && row === 0) ||
+       (col === 1 && row === 2)) { 
+         return false; 
+       }
+       
+    // check for diagonal
+    
   }
   
   function _checkForWin (event) {
     var row = event.srcElement.dataset.x;
-    var column = event.srcElement.dataset.y;
-    console.warn('row ::: ' , row);
-    console.warn('column ::: ' , column);
+    var col = event.srcElement.dataset.y;
     
-    if(_winningRow()) {
+    if(_winningRow(row)) {
       _boom();
-    } else if(_winningColumn()) {
+    } else if(_winningColumn(col)) {
       _boom();
-    } else if(_winningDiagonal()) {
+    } else if(_winningDiagonal(row, col)) {
       _boom();
     }
   }
   
   function _boom() {
     alert('BOOM! You won!');
-    // disable board
+    _finishGame();
   }
   
   function _playGame (event) {
@@ -48,8 +68,11 @@ var TicTacBoom = (function() {
     _checkForWin(event);
   } 
   
+  function _finishGame() {
+    board.onclick = null;
+  }
+  
   app.startGame = function(){
-    var board = document.getElementById("board");
     board.onclick = _playGame;
   };
   
